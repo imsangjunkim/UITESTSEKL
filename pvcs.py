496960f82ce4b5f2f15aa408d67e73b41b82dcc7
stup1db4nana <hweecheol.jeong@gmail.com>
2026-06-01 15:25:55 +0900

    update comments

--- pvcs.py
+++ pvcs.py
@@ -6,39 +6,36 @@ import shutil
 # 추가해야 할 사항: 파일 변경점 검사, 히스트에서 파일 되돌리기
 
 class Pvcs:
-    # 디렉토리 위치 설정
+    # 디렉토리 위치 설정.
     def __init__(self):
         self.HISTDIR = ".pvcshist"  # 옛 코밋 저장하는 위치
         self.CONFIGDIR = ".pvcsconfig"  # 추적하는 파일
         self.IGNOREDIR = ".pvcsignore"  # 무시할 파일
 
-    # 디렉토리 생성
+    # filepath를 받아 그 위치에 디렉토리 생성 .
     @staticmethod
     def create_dirs(filepath):
         if not os.path.exists(filepath):
             os.makedirs(filepath)
 
-    # 파일 생성
+    # filepath를 받아 그 위치에 파일 생성.
     @staticmethod
     def create_file(filepath):
         if not os.path.exists(filepath):
             file = open(filepath, "w")
             file.close()
 
-    # filepath 파일에서 compvalue를 검색 모드가 1일 경우 완전 일치, 0일 경우 시작하는 줄에 포함
+    # filepath에서 compvalue가 있는지 검사. 모드가 1일 경우 완전 일치, 0일 경우 시작하는 줄에 포함되는지 검사.
     @staticmethod
     def check_line_from_file(filepath, target, mode=1):
         if not os.path.exists(filepath):
             return False
 
         with open(filepath, "r") as file:
-            # 경로 노말라이즈
             normal_target = os.path.normpath(target.strip())
             for line in file:
-                # 공백 샵 무시
                 if not line.strip() or line.startswith("#"):
                     continue
-                # 경로 노말라이즈
                 normal_line = os.path.normpath(line.strip())
                 if mode == 1:
                     if normal_target == normal_line:
@@ -48,7 +45,7 @@ class Pvcs:
                         return True
         return False
 
-    # filepath에 new_line 추가
+    # filepath에 new_line 추가.
     @staticmethod
     def add_line_into_file(filepath, new_line):
         if not os.path.exists(filepath):
@@ -57,17 +54,17 @@ class Pvcs:
             file.write("\n" + new_line)
             return None
 
-    # 파일이 추적중인 경우 참, 아니면 거짓
+    # 파일이 추적중인 경우 참, 아니면 거짓.
     def check_tracking_status(self, filepath):
         normal_target = os.path.normpath(filepath)
         return self.check_line_from_file(self.CONFIGDIR, normal_target)
 
-    # 파일이 무시된 경우 참, 아니면 거짓
+    # 파일이 무시된 경우 참, 아니면 거짓. mode가 1인 경우 완전 일치, 0일 경우 시작줄에 포함되는지 검사.
     def check_ignore_status(self, filepath, mode):
         normal_target = os.path.normpath(filepath)
         return self.check_line_from_file(self.IGNOREDIR, normal_target, mode)
 
-    # 무시된 파일 제외 디렉토리 검사
+    # 무시된 파일을 제외하고 워킹 디렉토리를 탐색.
     def scan_pwd(self, pwd="."):
         scanned_files = []
 
@@ -82,6 +79,7 @@ class Pvcs:
 
         return scanned_files
 
+    # 추적 중인 파일만을 탐색
     def get_tracked_files(self, pwd="."):
         scanned_files = []
 
@@ -102,7 +100,7 @@ class Pvcs:
     def check_for_changes(self):
         return False
 
-    # 추적중인 파일을 iso날자로 구분해 hist에 저장
+    # 추적중인 파일을 iso날자로 구분해 hist에 디렉토리 째로 저장.
     def commit(self):
         foldername = datetime.datetime.now(datetime.timezone.utc).isoformat()
         foldername = foldername.replace(":", "-")
